7ea25c89b2afcb50d873928382f23af217fd364d
Misael Nava Reyes <misaelnavareyes@gmail.com>
2021-06-04 16:20:26 -0500

    Tercera edicion.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Entorno listo para trabajar el proyecto Propuestas Educativas ante el SARS-COV 2</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
